c8f95b738e17c297d2a079ab62edd65b145bc78d
Hamal Samuel <zariktechnologies@gmail.com>
2022-07-11 10:28:46 -0400

    updated js

--- public/index.js
+++ public/index.js
@@ -13,10 +13,12 @@ document.querySelectorAll('.nav-btn').forEach(n => n.addEventListener('click', (
 }))
 
 document.addEventListener("ready", function(){
-    document.querySelector("#name").addEventListener("change", function(){ 
-        document.querySelectorAll(".data").forEach(function(div){
-        div.style.display = "none";
-    })
+    console.log("It's Working") 
+        document.querySelector("#name").addEventListener("change", function(){
+            console.log("It's Working Again") 
+            document.querySelectorAll(".data").forEach(function(div){
+            div.style.display = "none";
+        })
     document.querySelector("#" + this.value).style.display = "block";
 })
 })
