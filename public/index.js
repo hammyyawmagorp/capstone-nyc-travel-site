c007b108be3e27ed5c1c8d859854a0bfc989c47b
Hamal Samuel <zariktechnologies@gmail.com>
2022-07-11 10:13:33 -0400

    updated html css & js

--- public/index.js
+++ public/index.js
@@ -11,3 +11,12 @@ document.querySelectorAll('.nav-btn').forEach(n => n.addEventListener('click', (
     hamburger.classList.remove('is-active');
     navMenu.classList.remove('is-active');
 }))
+
+document.addEventListener("ready", function(){
+    document.querySelector("#name").addEventListener("change", function(){ 
+        document.querySelectorAll(".data").forEach(function(div){
+        div.style.display = "none";
+    })
+    document.querySelector("#" + this.value).style.display = "block";
+})
+})
