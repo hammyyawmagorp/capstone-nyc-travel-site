c67e4fccc1c343127f2c4c75ef50a741582c7fa2
Hamal Samuel <zariktechnologies@gmail.com>
2022-07-15 18:56:28 -0400

    updated js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const cors = require('cors');
-
-
 app.use(express.json());
+
+const cors = require('cors');
 app.use(cors());
+
+
+const currentWeatherUrl = "https://api.weatherapi.com/v1/current.json?key=fffb84950e714159aa5160418221007&q=new york&aqi=yes"
+// baseUrl = "http://api.weatherapi.com/v1";
+// apiKey = fffb84950e714159aa5160418221007
+
+
+
+
+
+
+
 app.listen(4000, () => console.log ("Server running on port 4000"));
 
  
+
